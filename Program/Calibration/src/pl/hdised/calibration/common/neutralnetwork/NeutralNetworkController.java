e319d1c502cf11bae5efafbcb6bf8dc1536278d9
Sebastian Oprzędek (sebaopr806) <sebaopr806@student.polsl.pl>
2017-07-18 16:32:43 +0200

    neutral network p1

---
+++ Program/Calibration/src/pl/hdised/calibration/common/neutralnetwork/NeutralNetworkController.java
@@ -0,0 +1,7 @@
+package pl.hdised.calibration.common.neutralnetwork;
+
+/**
+ * Created by Sebastian Oprzędek on 18.07.2017.
+ */
+public class NeutralNetworkController {
+}
---
+++ Program/Calibration/src/pl/hdised/calibration/common/neutralnetwork/utils/TopologyHelper.java
@@ -0,0 +1,7 @@
+package pl.hdised.calibration.common.neutralnetwork.utils;
+
+/**
+ * Created by Sebastian Oprzędek on 18.07.2017.
+ */
+public class TopologyHelper {
+}
